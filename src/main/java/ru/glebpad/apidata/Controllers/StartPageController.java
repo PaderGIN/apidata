8750ff02e7b353d9b6d9a4f81f8c60d7a76cdc19
glebpad <glebpad@yandex.ru>
2023-01-22 14:26:15 +0300

    add: bear animation

--- src/main/java/ru/glebpad/apidata/Controllers/StartPageController.java
+++ src/main/java/ru/glebpad/apidata/Controllers/StartPageController.java
@@ -1,26 +1,13 @@
 package ru.glebpad.apidata.Controllers;
 
-import org.springframework.boot.autoconfigure.cassandra.CassandraProperties;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.codec.json.Jackson2JsonDecoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import javax.websocket.server.PathParam;
-import java.util.List;
 
 @Controller
 public class StartPageController {
     @GetMapping("/")
     public String hello(Model model) {
-        return "hello";
-    }
-
-    @PostMapping("/check")
-    public String check(@ModelAttribute String request) {
-        System.out.println((request));
-
-        return null;
+        return "main";
     }
 }
